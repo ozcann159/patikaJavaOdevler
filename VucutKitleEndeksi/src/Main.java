9199c18b85d8fc0be6c9d1e0d61eecf2bad128d7
ozcann159 <94390168+ozcann159@users.noreply.github.com>
2022-09-23 20:17:17 +0300

    Add files via upload

---
+++ VucutKitleEndeksi/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double boy, kilo , indeks;
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Lütfen boyunuzu (metre cinsinde) giriniz :");
+        boy = inp.nextDouble();
+        System.out.print("Lütfen kilonuzu giriniz :");
+        kilo = inp.nextDouble();
+        System.out.print("Vücut Kitle İndeksiniz :");
+
+
+        indeks = (kilo/ (boy * boy));
+        System.out.println("Vücut Kitle İndeksiniz :" + indeks);
+
+    }
+}
