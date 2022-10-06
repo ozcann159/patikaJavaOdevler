dbc362682191d7de6816cdf6f78c541a92320647
Elif Özcan <ozcann159@gmail.com>
2022-10-06 17:30:35 +0300

    Add files via upload

---
+++ harmonikSayi/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("n Sayısını giriniz : ");
+        int n = input.nextInt();
+        double result = 0;
+        for (int i = 1; i<=n; i++);{
+            result += (1/ i);
+        }
+        System.out.println(result);
+    }
+}
